7e93f2bb9305773d46ada1890360f1f414d41d9c
Alberto Barrilà <alberto.barrila@gmail.com>
2020-08-19 07:32:31 +0200

    aggiorna gli audio, aggiorna le vunerabilità

--- src/index.js
+++ src/index.js
@@ -5,73 +5,86 @@ const version = document.getElementById("js-version");
 version.innerHTML = package.version;
 
 ipcRenderer.on("play", (_, audio) => {
-	var player = new Audio(audio);
-	player.play();
+  var player = new Audio(audio);
+  player.play();
 });
 
 const progressCurrent = document.querySelectorAll(".js-progress-current");
 const drawProgress = () => {
-	progressCurrent.forEach(path => {
-		// Get the length of the path
-		let length = path.getTotalLength();
+  progressCurrent.forEach(path => {
+    // Get the length of the path
+    let length = path.getTotalLength();
 
-		// Just need to set this once manually on the .meter element and then can be commented out
-		// path.style.strokeDashoffset = length;
-		// path.style.strokeDasharray = length;
+    // Just need to set this once manually on the .meter element and then can be commented out
+    // path.style.strokeDashoffset = length;
+    // path.style.strokeDasharray = length;
 
-		// Get the value of the meter
-		let value = parseInt(path.parentNode.getAttribute("data-value"));
-		// Calculate the percentage of the total length
-		let to = length * ((100 - value) / 100);
-		// Trigger Layout in Safari hack https://jakearchibald.com/2013/animated-line-drawing-svg/
-		path.getBoundingClientRect();
-		// Set the Offset
-		path.style.strokeDashoffset = Math.max(0, to);
-	});
+    // Get the value of the meter
+    let value = parseInt(path.parentNode.getAttribute("data-value"));
+    // Calculate the percentage of the total length
+    let to = length * ((100 - value) / 100);
+    // Trigger Layout in Safari hack https://jakearchibald.com/2013/animated-line-drawing-svg/
+    path.getBoundingClientRect();
+    // Set the Offset
+    path.style.strokeDashoffset = Math.max(0, to);
+  });
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-	[].forEach.call(document.querySelectorAll(".js-audio"), function(el) {
-		el.addEventListener("click", () => {
-			el.classList.toggle("fa-volume-mute");
-			el.classList.toggle("fa-volume-up");
-			ipcRenderer.send("audioToggled");
-			//player.paused ? player.play() : player.pause();
-		});
-	});
+  [].forEach.call(document.querySelectorAll(".js-audio"), function(el) {
+    el.addEventListener("click", () => {
+      el.classList.toggle("fa-volume-mute");
+      el.classList.toggle("fa-volume-up");
+      ipcRenderer.send("audioToggled");
+      //player.paused ? player.play() : player.pause();
+    });
+  });
 
-	[].forEach.call(document.querySelectorAll(".js-settings"), function(el) {
-		el.addEventListener("click", () => {
-			const a = document.getElementById("js-panel");
-			a.classList.toggle("is-active");
-			el.classList.toggle("fa-times-circle");
-			el.classList.toggle("fa-sliders-h");
-		});
-	});
+  [].forEach.call(document.querySelectorAll(".js-settings"), function(el) {
+    el.addEventListener("click", () => {
+      const a = document.getElementById("js-panel");
+      a.classList.toggle("is-active");
+      el.classList.toggle("fa-times-circle");
+      el.classList.toggle("fa-sliders-h");
+    });
+  });
 
-	drawProgress();
+  drawProgress();
 });
 
 const quit = document.getElementById("js-quit");
 quit.addEventListener("click", () => {
-	ipcRenderer.send("quit");
+  ipcRenderer.send("quit");
 });
 
 const save = document.getElementById("js-save");
 const luxaforId = document.getElementById("luxaforId");
+const disabled = document.getElementById("disabled");
+const icon = document.getElementById("icon");
 
-storage.get("luxaforId", function(error, data) {
-	if (error) throw error;
-	luxaforId.value = data.id;
+storage.get("settings", function(error, data) {
+  if (error) throw error;
+  luxaforId.value = data.luxaforId;
+  disabled.checked = data.disabled;
 });
 
 save.addEventListener("click", () => {
-	const panel = document.getElementById("js-panel");
-	storage.set("luxaforId", { id: luxaforId.value }, error => {
-		if (error) throw error;
-		ipcRenderer.send("settingsSaved");
-	});
-	panel.classList.toggle("is-active");
+  const panel = document.getElementById("js-panel");
+  storage.set(
+    "settings",
+    {
+      luxaforId: luxaforId.value,
+      disabled: disabled.checked
+    },
+    error => {
+      if (error) throw error;
+      ipcRenderer.send("settingsSaved");
+      panel.classList.toggle("is-active");
+      icon.classList.toggle("is-active");
+      icon.classList.toggle("fa-times-circle");
+      icon.classList.toggle("fa-sliders-h");
+    }
+  );
 });
 
 const total = document.getElementById("js-step-total");
@@ -79,26 +92,37 @@ const progress = document.getElementById("js-progress");
 const progressTime = document.getElementById("js-progress-time");
 const stepName = document.getElementById("js-step-name");
 ipcRenderer.on("updateTimer", (_, timer) => {
-	progressTime.innerHTML = timer.time;
-	progress.setAttribute("data-value", timer.progress);
-	drawProgress();
-	if (
-		timer.type === "pause" ||
-		timer.type === "longpause" ||
-		timer.type === ""
-	) {
-		document.body.classList.add("is-pause");
-		stepName.innerHTML = "Pause";
-		total.innerHTML = "";
-	} else {
-		total.innerHTML = timer.total;
-		stepName.innerHTML = timer.name + " - ";
-		document.body.classList.remove("is-pause");
-	}
+  progressTime.innerHTML = timer.time;
+  progress.setAttribute("data-value", timer.progress);
+  drawProgress();
+  if (timer.type === "pause") {
+    document.body.classList.add("is-pause");
+    document.body.classList.remove("is-long-pause");
+    document.body.classList.remove("is-null");
+    stepName.innerHTML = "Pause";
+    total.innerHTML = "";
+  } else if (timer.type === "longpause") {
+    document.body.classList.remove("is-pause");
+    document.body.classList.add("is-long-pause");
+    document.body.classList.remove("is-null");
+    stepName.innerHTML = "Long Pause";
+    total.innerHTML = "";
+  } else if (timer.type === "") {
+    document.body.classList.remove("is-pause");
+    document.body.classList.remove("is-long-pause");
+    document.body.classList.add("is-null");
+    stepName.innerHTML = "";
+    total.innerHTML = "";
+  } else {
+    total.innerHTML = timer.total;
+    stepName.innerHTML = timer.name + " - ";
+    document.body.classList.remove("is-pause");
+    document.body.classList.remove("is-long-pause");
+    document.body.classList.remove("is-null");
+  }
 });
 
 const flash = document.getElementById("js-message");
 ipcRenderer.on("message", (_, text) => {
-	console.log(text);
-	flash.innerHTML = text;
+  flash.innerHTML = text;
 });
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const {
-	app,
-	Tray,
-	BrowserWindow,
-	ipcMain,
-	Notification,
-	Menu
+  app,
+  Tray,
+  BrowserWindow,
+  ipcMain,
+  Notification,
+  Menu
 } = require("electron");
 
 const path = require("path");
@@ -16,6 +16,7 @@ const storage = require("electron-json-storage");
 const { autoUpdater } = require("electron-updater");
 
 const package = require("../package.json");
+const { spawn } = require("child_process");
 
 let window = undefined;
 const timers = require("./timers.json");
@@ -27,287 +28,313 @@ let icon;
 
 let playSound = true;
 let luxaforId = null;
+let disabled = false;
 
-storage.get("luxaforId", function (error, data) {
-	if (error) throw error;
-	luxaforId = data.id;
+storage.get("settings", function(error, data) {
+  if (error) throw error;
+  luxaforId = data.luxaforId;
+  disabled = data.disabled;
 });
 
 if (process.platform === "darwin") {
-	icon = path.resolve(__dirname, "images/pomdor-mac.png");
+  icon = path.resolve(__dirname, "images/pomdor-mac.png");
 } else {
-	icon = path.resolve(__dirname, "images/pomdor-win-black.png");
+  icon = path.resolve(__dirname, "images/pomdor-win-black.png");
 }
 
 const updateTimers = () => {
-	timers.forEach(t => {
-		t.startMoment = moment(t.start, "LTS");
-		t.endMoment = moment(t.end, "LTS");
-	});
+  timers.forEach(t => {
+    t.startMoment = moment(t.start, "LTS");
+    t.endMoment = moment(t.end, "LTS");
+  });
 };
 updateTimers();
 
 const createTray = () => {
-	tray = new Tray(icon);
-	tray.setToolTip("Pomdor");
-	if (process.platform !== "darwin") {
-		const contextMenu = Menu.buildFromTemplate([
-			{
-				label: "Version: " + package.version
-			},
-			{
-				label: "Quit",
-				click: () => {
-					app.quit();
-				}
-			}
-		]);
-		tray.setContextMenu(contextMenu);
-	}
+  tray = new Tray(icon);
+  tray.setToolTip("Pomdor");
+  if (process.platform !== "darwin") {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "Version: " + package.version
+      },
+      {
+        label: "Quit",
+        click: () => {
+          app.quit();
+        }
+      }
+    ]);
+    tray.setContextMenu(contextMenu);
+  }
 
-	tray.on("click", function (event) {
-		toggleWindow();
-	});
+  tray.on("click", function(event) {
+    toggleWindow();
+  });
 };
 
 const setNull = () => {
-	tray.setTitle("🕹️");
-	if (process.platform !== "darwin") {
-		tray.setToolTip("🕹️");
-		tray.setImage(path.resolve(__dirname, "images/pomdor-win-black.png"));
-	}
+  tray.setTitle("🕹️");
+  if (process.platform !== "darwin") {
+    tray.setToolTip("🕹️");
+    tray.setImage(path.resolve(__dirname, "images/pomdor-win-black.png"));
+  }
 };
 
 const setWork = (name, time) => {
-	tray.setTitle("🍅 " + colorActive + time);
+  tray.setTitle("🍅 " + colorActive + time);
 
-	if (process.platform !== "darwin") {
-		tray.setToolTip(name + " - " + time);
-		tray.setImage(path.resolve(__dirname, "images/pomdor-win-red.png"));
-	}
+  if (process.platform !== "darwin") {
+    tray.setToolTip(name + " - " + time);
+    tray.setImage(path.resolve(__dirname, "images/pomdor-win-red.png"));
+  }
 };
 
 const setPause = (name, time) => {
-	tray.setTitle("🍌 " + colorPause + time);
-	if (process.platform !== "darwin") {
-		tray.setToolTip(name + " - " + time);
-		tray.setImage(path.resolve(__dirname, "images/pomdor-win-green.png"));
-	}
+  tray.setTitle("🍌 " + colorPause + time);
+
+  if (process.platform !== "darwin") {
+    tray.setToolTip(name + " - " + time);
+    tray.setImage(path.resolve(__dirname, "images/pomdor-win-green.png"));
+  }
 };
 
 const notify = (time, type) => {
-	if (time === "00:01") {
-		var notification = new Notification({
-			title: "Pomdor",
-			body:
-				type == "pause" || type == "longpause"
-					? "🍅 Pomodor"
-					: "🍌 Pause",
-			silent: true,
-			icon: icon
-		});
-		notification.show();
-		let audio = path.join(__dirname, "audio", "pause-vic.mpeg");
-		if (type == "pause") audio = path.join(__dirname, "audio", "work-ale.wav");
-		if (type == "longpause")
-			audio = path.join(__dirname, "audio", "longpause.m4a");
-		if (playSound) window.webContents.send("play", audio);
-	}
+  if (time === "00:01") {
+    var notification = new Notification({
+      title: "Pomdor",
+      body: type == "pause" || type == "longpause" ? "🍅 Pomodor" : "🍌 Pause",
+      silent: true,
+      icon: icon
+    });
+    notification.show();
+    let audio = path.join(__dirname, "audio", "pause.wav");
+    if (type == "pause") audio = path.join(__dirname, "audio", "work.wav");
+    if (type == "longpause")
+      audio = path.join(__dirname, "audio", "longpause.m4a");
+    if (playSound) window.webContents.send("play", audio);
+  }
 };
 
 function update() {
-	let date = moment();
-	let timer = null;
+  let date = moment();
+  let timer = null;
+  if (disabled || date.day() === 0 || date.day() === 6) {
+    setNull();
+    window.webContents.send("updateTimer", {
+      time: "00:00",
+      type: "",
+      name: "",
+      total: "",
+      progress: 0
+    });
+    return;
+  }
 
-	if (date.day === 0 || date.day === 6)
-		setNull();
+  if (timers[0].startMoment.date() != date.date()) updateTimers();
 
-	if (timers[0].startMoment.date() != date.date()) updateTimers();
+  timers.forEach(t => {
+    if (date.isBetween(t.startMoment, t.endMoment)) timer = t;
+  });
 
-	timers.forEach(t => {
-		if (date.isBetween(t.startMoment, t.endMoment)) timer = t;
-	});
+  let time = timer
+    ? moment(timer.endMoment.diff(date)).format("mm:ss")
+    : "00:00";
+  let type = timer ? timer.type : "";
+  let name = timer ? timer.name : "";
+  let progress = 0;
+  if (timer)
+    progress = Math.round(
+      (moment(timer.endMoment.diff(date)) * 100) /
+        moment(timer.endMoment.diff(timer.startMoment))
+    );
 
-	let time = timer
-		? moment(timer.endMoment.diff(date)).format("mm:ss")
-		: "00:00";
-	let type = timer ? timer.type : "";
-	let name = timer ? timer.name : "";
-	let progress = 0;
-	if (timer)
-		progress = Math.round(
-			(moment(timer.endMoment.diff(date)) * 100) /
-			moment(timer.endMoment.diff(timer.startMoment))
-		);
+  let typeTimers = timers.filter(t => t.type == type);
 
-	let typeTimers = timers.filter(t => t.type == type);
+  window.webContents.send("updateTimer", {
+    time: time,
+    type: type,
+    name: name,
+    total: `${typeTimers.indexOf(timer) + 1}/${typeTimers.length}`,
+    progress: progress
+  });
 
-	window.webContents.send("updateTimer", {
-		time: time,
-		type: type,
-		name: name,
-		total: `${typeTimers.indexOf(timer) + 1}/${typeTimers.length}`,
-		progress: progress
-	});
+  if (luxaforId && type !== status) {
+    status = type;
 
-	if (luxaforId && type !== status) {
-		status = type;
+    let actionFields = {
+      color: "custom",
+      custom_color: "000000"
+    };
 
-		let actionFields = {
-			color: "custom",
-			custom_color: "000000"
-		};
+    if (type === "work") {
+      actionFields = {
+        color: "red"
+      };
+    }
 
-		if (type === "work")
-			actionFields = {
-				color: "red"
-			};
-		if (type === "pause")
-			actionFields = {
-				color: "green"
-			};
-		if (type === "longpause")
-			actionFields = {
-				color: "yellow"
-			};
+    if (type === "pause") {
+      actionFields = {
+        color: "green"
+      };
+    }
 
-		const data = {
-			userId: luxaforId,
-			actionFields
-		};
+    if (type === "longpause") {
+      if (programPaths.length > 0)
+        programPaths.forEach(p => {
+          console.log("path", p);
+          let program = spawn(p.path);
+          program.on("close", (code, signal) => {
+            console.log(
+              `${p.name} process terminated due to receipt of signal ${signal}`
+            );
+          });
+          programs.push(program);
+        });
 
-		request
-			.post("https://api.luxafor.com/webhook/v1/actions/solid_color")
-			.set("Content-Type", "application/json")
-			.send(data)
-			.then(res => console.log(res.body))
-			.catch(err => console.log(err));
-	}
+        actionFields = {
+        color: "yellow"
+      };
+    }
 
-	if (timer === null) setNull();
-	else if (type === "work") setWork(name, time);
-	else if (type === "pause" || type === "longpause") setPause(name, time);
-	notify(time, type);
+    const data = {
+      userId: luxaforId,
+      actionFields
+    };
+
+    request
+      .post("https://api.luxafor.com/webhook/v1/actions/solid_color")
+      .set("Content-Type", "application/json")
+      .send(data)
+      .then(res => console.log(res.body))
+      .catch(err => console.log(err));
+  }
+
+  if (timer === null) setNull();
+  else if (type === "work") setWork(name, time);
+  else if (type === "pause" || type === "longpause") setPause(name, time);
+  notify(time, type);
 }
 
 if (process.platform === "darwin") app.dock.hide();
 
 app.on("ready", () => {
-	createTray();
-	createWindow();
-	setInterval(update, 1000);
+  createTray();
+  createWindow();
+  setInterval(update, 1000);
 });
 
 const getWindowPosition = () => {
-	const windowBounds = window.getBounds();
-	const trayBounds = tray.getBounds();
-	if (process.platform === "darwin")
-		return {
-			x: Math.round(trayBounds.x - windowBounds.width / 2),
-			y: Math.round(trayBounds.y + trayBounds.height + 4)
-		};
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+  if (process.platform === "darwin")
+    return {
+      x: Math.round(trayBounds.x - windowBounds.width / 2),
+      y: Math.round(trayBounds.y + trayBounds.height + 4)
+    };
 
-	return {
-		x: Math.round(trayBounds.x - windowBounds.width / 2),
-		y: Math.round(trayBounds.y - windowBounds.height)
-	};
+  return {
+    x: Math.round(trayBounds.x - windowBounds.width / 2),
+    y: Math.round(trayBounds.y - windowBounds.height)
+  };
 };
 
 const createWindow = () => {
-	window = new BrowserWindow({
-		width: 320,
-		height: 450,
-		show: false,
-		frame: false,
-		fullscreenable: false,
-		resizable: false,
-		transparent: true,
-		hasShadow: false,
-		skipTaskbar: true,
-		webPreferences: {
-			nodeIntegration: true,
-			backgroundThrottling: false
-		}
-	});
-	window.loadURL(`file://${path.join(__dirname, "index.html")}`);
+  window = new BrowserWindow({
+    width: 320,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    hasShadow: false,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      backgroundThrottling: false
+    }
+  });
+  window.loadURL(`file://${path.join(__dirname, "index.html")}`);
 };
 
 const toggleWindow = () => {
-	window.isVisible() ? window.hide() : showWindow();
+  window.isVisible() ? window.hide() : showWindow();
 };
 
 const showWindow = () => {
-	const position = getWindowPosition();
-	window.setPosition(position.x, position.y);
-	window.show();
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y);
+  window.show();
 };
 
 ipcMain.on("show-window", () => {
-	showWindow();
+  showWindow();
 });
 
 ipcMain.on("quit", () => {
-	app.quit();
+  app.quit();
 });
 
 ipcMain.on("audioToggled", () => {
-	playSound = !playSound;
+  playSound = !playSound;
 });
 
 ipcMain.on("settingsSaved", () => {
-	storage.get("luxaforId", function (error, data) {
-		if (error) throw error;
-		luxaforId = data.id;
-	});
+  storage.get("settings", function(error, data) {
+    if (error) throw error;
+    luxaforId = data.luxaforId;
+    disabled = data.disabled;
+  });
 });
 
 autoUpdater.on("checking-for-update", () => {
-	window.webContents.send(
-		"message",
-		"verifica presenza nuovi aggiornamenti..."
-	);
+  window.webContents.send(
+    "message",
+    "verifica presenza nuovi aggiornamenti..."
+  );
 });
 
 autoUpdater.on("update-available", info => {
-	window.webContents.send("message", "aggiornamento disponibile.");
+  window.webContents.send("message", "aggiornamento disponibile.");
 });
 
 autoUpdater.on("update-not-available", info => {
-	window.webContents.send("message", "la tua versione è aggiornata.");
+  window.webContents.send("message", "la tua versione è aggiornata.");
 });
 
 autoUpdater.on("error", err => {
-	window.webContents.send("message", "errore aggiornamento. " + err);
+  window.webContents.send("message", "errore aggiornamento. " + err);
 });
 
 autoUpdater.on("download-progress", progressObj => {
-	window.webContents.send(
-		"message",
-		"scaricamento in corso " + progressObj.percent + "%"
-	);
+  window.webContents.send(
+    "message",
+    "scaricamento in corso " + progressObj.percent + "%"
+  );
 });
 
 autoUpdater.on("update-downloaded", info => {
-	autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();
 });
 
-app.on("ready", function () {
-	autoUpdater.checkForUpdates();
+app.on("ready", function() {
+  autoUpdater.checkForUpdates();
 });
 
 app.on("before-quit", event => {
-	if (luxaforId) {
-		request
-			.post("https://api.luxafor.com/webhook/v1/actions/solid_color")
-			.set("Content-Type", "application/json")
-			.send({
-				userId: luxaforId,
-				actionFields: {
-					color: "custom",
-					custom_color: "000000"
-				}
-			})
-			.then(res => console.log(res.body))
-			.catch(err => console.log(err));
-	}
+  if (luxaforId) {
+    request
+      .post("https://api.luxafor.com/webhook/v1/actions/solid_color")
+      .set("Content-Type", "application/json")
+      .send({
+        userId: luxaforId,
+        actionFields: {
+          color: "custom",
+          custom_color: "000000"
+        }
+      })
+      .then(res => console.log(res.body))
+      .catch(err => console.log(err));
+  }
 });
